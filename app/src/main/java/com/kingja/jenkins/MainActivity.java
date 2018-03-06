82cbf9958411e09ad67085c9399f48a3b0dc4612
KingJA <kingjavip@gmail.com>
2018-03-06 16:36:36 +0800

    add paramter

--- app/src/main/java/com/kingja/jenkins/MainActivity.java
+++ app/src/main/java/com/kingja/jenkins/MainActivity.java
@@ -1,7 +1,8 @@
 package com.kingja.jenkins;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tv_version_name = findViewById(R.id.tv_version_name);
+        TextView tv_version_code = findViewById(R.id.tv_version_code);
+        TextView tv_build_type = findViewById(R.id.tv_build_type);
+        TextView tv_build_date = findViewById(R.id.tv_build_date);
+        tv_version_name.setText(BuildConfig.VERSION_NAME);
+        tv_version_code.setText(String.valueOf(BuildConfig.VERSION_CODE));
+        tv_build_type.setText(BuildConfig.BUILD_TYPE);
+        tv_build_date.setText(BuildConfig.BUILD_DATE);
+
+//        Toast.makeText(this,BuildConfig.HOST_URL,Toast.LENGTH_SHORT).show();
     }
 }
