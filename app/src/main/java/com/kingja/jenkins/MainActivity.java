dfa7d88b212d9e98fbd3f48fbb85db3cb3fd1c69
KingJA <kingjavip@gmail.com>
2018-03-16 13:50:32 +0800

    add HOST_URL

--- app/src/main/java/com/kingja/jenkins/MainActivity.java
+++ app/src/main/java/com/kingja/jenkins/MainActivity.java
@@ -14,10 +14,12 @@ public class MainActivity extends AppCompatActivity {
         TextView tv_version_code = findViewById(R.id.tv_version_code);
         TextView tv_build_type = findViewById(R.id.tv_build_type);
         TextView tv_build_date = findViewById(R.id.tv_build_date);
+        TextView tv_host_url = findViewById(R.id.tv_host_url);
         tv_version_name.setText(BuildConfig.VERSION_NAME);
         tv_version_code.setText(String.valueOf(BuildConfig.VERSION_CODE));
         tv_build_type.setText(BuildConfig.BUILD_TYPE);
         tv_build_date.setText(BuildConfig.BUILD_DATE);
+        tv_host_url.setText(BuildConfig.HOST_URL);
 
 //        Toast.makeText(this,BuildConfig.HOST_URL,Toast.LENGTH_SHORT).show();
     }
